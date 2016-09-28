eea8cf31950224cee69114dc1339fad110cf037d
maxwen <max.weninger@gmail.com>
2016-09-29 00:41:15 +0200

    mido: add mediaextractor seccomp policy
    Change-Id: I04d709f88db4258658991cbeacccd3b399c662f8

--- device.mk
+++ device.mk
@@ -313,7 +313,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
