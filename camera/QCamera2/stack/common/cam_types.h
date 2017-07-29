528ac402350122111e83156405a69491ccc454a8
Sultanxda <sultanxda@gmail.com>
2017-07-29 13:33:25 -0700

    QCamera2: Set auto antibanding mode using country ISO code
    This hardcodes a list of countries using a 60 Hz power grid, and sets the
    antibanding mode based on whether or not the country reported by the
    framework is in that list. There are far more 50 Hz countries than 60 Hz
    countries, so the default setting when the country isn't known is 50 Hz.
    This is also the reason why only the 60 Hz countries are enumerated (since
    there are fewer of them).
    
    Change-Id: Id77e4ff2c1e227cdc43bf31021caa13a44031614

--- camera/QCamera2/stack/common/cam_types.h
+++ camera/QCamera2/stack/common/cam_types.h
@@ -45,6 +45,8 @@
 #define BHIST_STATS_DEBUG_DATA_SIZE       (70000)
 #define TUNING_INFO_DEBUG_DATA_SIZE       (4)
 
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
+
 #define CEILING64(X) (((X) + 0x0003F) & 0xFFFFFFC0)
 #define CEILING32(X) (((X) + 0x0001F) & 0xFFFFFFE0)
 #define CEILING16(X) (((X) + 0x000F) & 0xFFF0)
