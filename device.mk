920e4576efba3efa97288989cece2400b313b9be
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-12-17 22:50:15 +0800

    mido: update proprietary files list fom tissot
    * update all blobs but graphics
    * update paths for wcnss daemon and config
    * update audio_effects config

--- device.mk
+++ device.mk
@@ -356,7 +356,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
