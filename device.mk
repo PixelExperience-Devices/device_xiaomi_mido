fc0ddbfa7daf79ccdf9802475f49149234746da3
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-01-13 16:49:47 +0800

    mido: switch to oss telephony-ext

--- device.mk
+++ device.mk
@@ -373,6 +373,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
+PRODUCT_PACKAGES += \
+    telephony-ext
+
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
