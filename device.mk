ae7e31b3cb5d45be65c7a96afeb804467dd988ff
DevStark <devstark15@gmail.com>
2018-01-16 18:34:20 +0000

    Add PixelStyle

--- device.mk
+++ device.mk
@@ -377,5 +377,12 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Doze
+
 PRODUCT_PACKAGES += \
     XiaomiDoze
+# Google Lens
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
