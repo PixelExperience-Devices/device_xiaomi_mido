8dcfbf2a888600cf39d1c6902aa67f90d77b75ef
Nikhil <5293647+niikhill@users.noreply.github.com>
2018-02-18 12:37:40 +0000

    Revert "mido: switch to oss telephony-ext"
    This reverts commit fc0ddbfa7daf79ccdf9802475f49149234746da3.

--- device.mk
+++ device.mk
@@ -370,9 +370,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
-PRODUCT_PACKAGES += \
-    telephony-ext
-
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
