2adb115746a7fa2a0e8afc2f360006e22d1eaf9f
Offain <fabian.leutenegger@bluewin.ch>
2019-02-06 17:40:52 +0000

    mido: Adapt ANT+ for CAF

--- device.mk
+++ device.mk
@@ -34,7 +34,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -76,7 +75,8 @@ PRODUCT_COPY_FILES += \
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
+    antradio_app \
+    libantradio
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -95,6 +95,9 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
     tinymix
+    AntHalService \
+    antradio_app \
+    libantradio
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
