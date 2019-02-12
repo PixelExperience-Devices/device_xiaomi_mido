3bc265b7e8e6960363d862b026574185d12f1f05
Aryan <arykedare@gmail.com>
2019-02-12 20:14:58 +0530

    mido: PixelExperience Bringup

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
