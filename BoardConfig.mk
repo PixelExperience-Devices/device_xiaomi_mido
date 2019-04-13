631d5a107f819269daaaa6e1be086aa18becdce9
Aryan <arykedare@gmail.com>
2019-04-13 11:05:20 +0530

    Revert "mido: lineagehw: Transition to vendor.lineage.touch"
    This reverts commit 5b6ba6f7f314a45e9235b93d19d22f98f24411cc.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,9 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
+# Lineage Hardware
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
