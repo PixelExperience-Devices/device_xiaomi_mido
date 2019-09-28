73931cd4c14e4c8994529144f9589bbe9cff3fa3
iheyuhui <865963285@qq.com>
2019-09-28 13:03:09 +0000

    set SELINUX_IGNORE_NEVERALLOWS := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,6 +200,7 @@ VENDOR_SECURITY_PATCH := 2019-04-01
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
