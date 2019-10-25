f7556db653c2193ff8eb1046c4baa8a51a3cbf36
Murat Kozan <muratkozan350@gmail.com>
2019-10-25 23:46:57 +0300

    mido: Fix power hal

--- device.mk
+++ device.mk
@@ -271,7 +271,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
