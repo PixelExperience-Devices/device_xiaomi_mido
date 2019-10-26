48dcccbe701cb33437cd3ccd6da33d31b4474044
akhilnarang <akhilnarang@users.noreply.github.com>
2019-10-26 10:23:45 +0000

    Some Product Packages For RIL

--- device.mk
+++ device.mk
@@ -312,7 +312,14 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     libxml2 \
     telephony-ext \
-    ims-ext-common_system
+    ims-ext-common_system \
+    rild \
+    libprotobuf-cpp-full \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    ims_ext_common.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext \
