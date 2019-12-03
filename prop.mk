7db5ffec01337b052477ed85814e19ff7f8df75a
Dyneteve <sound0020@gmail.com>
2019-12-03 11:43:55 +0100

    mido: Kang some system props from coral

--- prop.mk
+++ prop.mk
@@ -177,8 +177,9 @@ service.qti.ims.enabled=1
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.enable_gl_backpressure=1 \
+debug.sf.early_phase_offset_ns=500000 \
+debug.sf.early_app_phase_offset_ns=500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
 debug.sf.early_gl_app_phase_offset_ns=15000000 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
