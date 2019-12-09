24bcb48395a26c47ba2545292799642577b4a5b7
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-12-09 19:59:24 +0100

    mido: build libunwind for gx_fpd
    Change-Id: If1d1bc2f9d8cb863b5215e6b981f00c88f4430a4
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -170,7 +170,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1 \
+    libunwind
 
 # FM
 PRODUCT_PACKAGES += \
