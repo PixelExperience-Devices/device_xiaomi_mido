2924def4edd09d2a9264cfa54e4c8241eb833f7a
ZeeLog <Danjel.Zammit@gmail.com>
2020-08-04 12:23:22 +0200

    mido: Correct TARGET_RECOVERY_DEVICE_MODULES
    Change-Id: I5736a88760985e43283c5d4ac6a28f6d6fcdb445

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,7 +158,7 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_mido
-TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
+TARGET_RECOVERY_DEVICE_MODULES := libinit_mido
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
