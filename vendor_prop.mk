55d538dadfd561dcf63e6f4ecf90eb8a9a3b9cf6
GiaSen <giammasena@gmail.com>
2020-11-15 18:46:49 +0100

    mido: Update coral fp to nov

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -4,7 +4,7 @@
 
 # Pixel identification
 PRODUCT_PRODUCT_PROPERTIES += \
-ro.build.fingerprint=google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys
+ro.build.fingerprint=google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
