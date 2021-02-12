297ab7236eb56198ae8445977006a69d60336cad
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-12 09:17:13 +0100

    mido: goodix: drop libunwind & libbacktrace deps

--- device.mk
+++ device.mk
@@ -187,8 +187,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    libunwind
+    android.hardware.biometrics.fingerprint@2.1
 
 # FM
 PRODUCT_PACKAGES += \
