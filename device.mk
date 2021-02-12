bcc124de8f92b9e1dc0bf693b09478143c67e93c
ZeeLog <Danjel.Zammit@gmail.com>
2021-02-12 11:48:52 +0100

    mido: Update packages for WFD
    * required after VNDK support enablement

--- device.mk
+++ device.mk
@@ -169,13 +169,14 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
-    libdisplayconfig.qti.vendor \
+    libdisplayconfig.qti \
     liboverlay \
     libgenlock \
     libqdMetaData \
     libtinyxml \
     libvulkan \
-    vendor.display.config@1.0.vendor
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@2.0
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
@@ -307,7 +308,8 @@ PRODUCT_PACKAGES += \
     libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_enc_common
 
 # Power
 PRODUCT_PACKAGES += \
