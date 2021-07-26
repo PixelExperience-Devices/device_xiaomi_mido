3d2c62ed3b7a0c9a857a71e8d583213eef866327
Suren Baghdasaryan <surenb@google.com>
2021-07-26 15:38:29 +0200

    lmkd: Set lmkd configuration properties Bug: 63631020
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- device.mk
+++ device.mk
@@ -281,8 +281,6 @@ PRODUCT_PACKAGES += \
 # LMKD
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.log_stats=true
 
 # Media
 PRODUCT_COPY_FILES += \
