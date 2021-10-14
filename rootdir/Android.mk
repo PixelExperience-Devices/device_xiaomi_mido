0458834e6b6d92aa348fe3a93f1d31a8ac3f0d55
Ethan Chen <intervigil@gmail.com>
2021-10-14 11:27:45 +0400

    mido: Import basic USB HAL 1.0
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -67,10 +67,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.usb.rc
+LOCAL_MODULE       := init.mido.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
+LOCAL_SRC_FILES    := etc/init.mido.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
