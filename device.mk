9cea38399f025dc097bd2280dca8b8594f45061d
jhenrique09 <jhsv09@gmail.com>
2022-01-14 10:46:46 -0300

    mido: Remove in-tree hals
    Revert "mido: prepare to move to in-tree HALs"
    
    This reverts commit 6ec3ad827bb88dc62f45b61d4ae5ab451f9b735d.

--- device.mk
+++ device.mk
@@ -37,9 +37,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Sna
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-    
-QCOM_SOONG_NAMESPACE := \
-    $(LOCAL_PATH)/qcom-caf
 
 # Permissions
 PRODUCT_COPY_FILES += \
