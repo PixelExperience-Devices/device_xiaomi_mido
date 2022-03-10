7c6218ab1168d5db4b3d87fbb08c53a9b96b0f99
Georg Veichtlbauer <georg@vware.at>
2022-03-10 16:03:55 +0100

    mido: Remove CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,6 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
