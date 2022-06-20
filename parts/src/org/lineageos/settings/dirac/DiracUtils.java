922a2c09db903b04048d0f0b3d503f880c6553c9
NRanjan-17 <nalinishranjan05@gmail.com>
2022-06-20 08:57:57 +0530

    mido: Dirac: drop duplicate code
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -72,22 +72,6 @@ public final class DiracUtils {
         }
     }
 
-    protected void refreshPlaybackIfNecessary(){
-        if (mMediaSessionManager == null) {
-            mMediaSessionManager = (MediaSessionManager) mContext.getSystemService(Context.MEDIA_SESSION_SERVICE);
-        }
-        final List<MediaController> sessions
-                = mMediaSessionManager.getActiveSessionsForUser(
-                null, UserHandle.ALL);
-        for (MediaController aController : sessions) {
-            if (PlaybackState.STATE_PLAYING ==
-                    getMediaControllerPlaybackState(aController)) {
-                triggerPlayPause(aController);
-                break;
-            }
-        }
-    }
-
     private void triggerPlayPause(MediaController controller) {
         long when = SystemClock.uptimeMillis();
         final KeyEvent evDownPause = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PAUSE, 0);
