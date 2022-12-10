8de19f50d7c5e82f7a89ee12f1a0228019cd596f
NRanjan-17 <nalinishranjan05@gmail.com>
2022-12-10 18:22:08 +0530

    [SQUASH] Revert vibration intensity toggle [temp]
    Revert "mido: parts: Add vibration intensity control"
    
    This reverts commit 22a8d6c210bc3b44fcebefbbfc5d14d71565bafe.
    
    Revert "mido: parts: Update translations"
    
    This reverts commit fee270a48c425b3a4eae98a7678192a3f9e7352e.
    
    Revert "mido: parts: Fix vibrator intensity main override switch"
    
    This reverts commit 0ac65b43c85598542e150a2dff50256bcf51fbb1.
    
    Revert "mido: parts: add missing code"
    
    This reverts commit 59e8da674f1fc05721f95eb86e665cb103478317.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2022 The LineageOS Project
+ *               2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,35 +20,19 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 import android.provider.Settings;
-
-import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.preferences.FileUtils;
-import org.lineageos.settings.preferences.SecureSettingSwitchPreference;
 import org.lineageos.settings.soundcontrol.SoundControlSettings;
 import org.lineageos.settings.torch.TorchSettings;
-import org.lineageos.settings.vibration.VibratorStrengthPreference;
-import org.lineageos.settings.vibration.VibratorSettings;
-import org.lineageos.settings.vibration.VibratorOverrideModeSwitch;
 
 public class BootCompletedReceiver extends BroadcastReceiver implements Controller {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
-
-    private void restore(String file, boolean enabled) {
-        if (file == null) {
-            return;
-        }
-        if (enabled) {
-            FileUtils.setValue(file, "1");
-        }
-    }
 
     @Override
     public void onReceive(final Context context, Intent intent) {
@@ -58,16 +42,13 @@ public class BootCompletedReceiver extends BroadcastReceiver implements Controll
         }
         new DiracUtils(context).onBootCompleted();
 
-        VibratorStrengthPreference.restore(context);
-
         FileUtils.setValue(TorchSettings.TORCH_1_BRIGHTNESS_PATH,
                 Settings.Secure.getInt(context.getContentResolver(),
                         TorchSettings.KEY_WHITE_TORCH_BRIGHTNESS, 100));
         FileUtils.setValue(TorchSettings.TORCH_2_BRIGHTNESS_PATH,
                 Settings.Secure.getInt(context.getContentResolver(),
                         TorchSettings.KEY_YELLOW_TORCH_BRIGHTNESS, 100));
-
-        int gain = Settings.Secure.getInt(context.getContentResolver(),
+    int gain = Settings.Secure.getInt(context.getContentResolver(),
                 SoundControlSettings.PREF_HEADPHONE_GAIN, 4);
         FileUtils.setValue(SoundControlSettings.HEADPHONE_GAIN_PATH, gain + " " + gain);
         FileUtils.setValue(SoundControlSettings.MICROPHONE_GAIN_PATH, Settings.Secure.getInt(context.getContentResolver(),
@@ -75,12 +56,6 @@ public class BootCompletedReceiver extends BroadcastReceiver implements Controll
         FileUtils.setValue(SoundControlSettings.SPEAKER_GAIN_PATH, Settings.Secure.getInt(context.getContentResolver(),
                 SoundControlSettings.PREF_SPEAKER_GAIN, 0));
 
-        boolean enabled = false;
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        enabled = sharedPrefs.getBoolean(VibratorSettings.PREF_VMAX_OVERRIDE_SWITCH, false);
-        restore(VibratorOverrideModeSwitch.getFile(), enabled);
-
-        // KCAL
         if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
             FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
                     PREF_ENABLED, 0));
